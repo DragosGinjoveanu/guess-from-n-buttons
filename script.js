f217f652d110d0112e220fcdee815ab683290a24
Ginjoveanu Dragos <82390006+DragosGinjoveanu@users.noreply.github.com>
2021-05-05 16:35:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,35 @@
-      var random;
-      // this function adds the given number of buttons and picks a random number between 1 and it
-      function addButtons() {
-          var text = document.getElementById("myText").value;
-          var numberButtons = parseInt(text);
-          if (numberButtons <= 0) {
-            alert("Please write another number");
-            restartGame();
-          } else if (numberButtons == 1) {
-            alert("It's too hard!");
-            restartGame();
-          } else {
-            random = Math.floor(Math.random() * numberButtons) + 1;
-            for (var i = 1; i <= numberButtons; i++) {
-                $('#buttons').append(`
-                  <button type="button" id = "` + i + `" class="btn btn-secondary btn-lg" onclick = "return guessButton(` + i + `);">Button: ` + i + `</button>
-                  `);
-            }
-            return false;
-          }
-      }
-      // this function verifies if the clicked button is winner or not
-      function guessButton(id) {
-        if (id == random) {
-            swal({
-            text: "Winner!",
-            icon: "success",
-            });
-        } else {
-            swal({
-            text: "Loser!",
-            icon: "error",
-            });
-        }
-      }
-      // this function restarts the whole game
-      function restartGame() {
-        location.reload();
-      }
+var random;
+// this function adds the given number of buttons and picks a random number between 1 and it
+function addButtons() {
+  var text = document.getElementById("myText").value;
+  var numberButtons = parseInt(text);
+  if (numberButtons <= 0) {
+    alert("Please write another number");
+    restartGame();
+  } else if (numberButtons == 1) {
+    alert("It's too hard!");
+    restartGame();
+  } else {
+    random = Math.floor(Math.random() * numberButtons) + 1;
+    for (var i = 1; i <= numberButtons; i++) {
+      $('#buttons').append(`
+        <button type="button" id = "` + i + `" class="btn btn-secondary btn-lg" onclick = "return guessButton(` + i + `);">Button: ` + i + `</button>
+      `);
+    }
+      return false;
+  }
+}
+// this function verifies if the clicked button is winner or not
+function guessButton(id) {
+  if (id == random) {
+    document.getElementById("status").style.color = "green";
+    document.getElementById("status").innerHTML = "Status: Winner!!!";
+  } else {
+    document.getElementById("status").style.color = "red";
+    document.getElementById("status").innerHTML = "Status: Loser!!!";
+  }
+}
+// this function restarts the whole game
+function restartGame() {
+  location.reload();
+}
